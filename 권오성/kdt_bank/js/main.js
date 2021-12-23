8e62667212227f14e74aae541dc8ff86da8d328f
05Castle <kos4005@gamil.com>
2021-12-23 16:46:53 +0900

    feat: 계좌 이름, 계좌 정보, 저금통에 관한 json 연동
    헤더 부분 계좌 이름 연동하여 텍스트 변경
    계좌 금액 연동하여 텍스트 변경
    바 그래프 %에 따라 변동하도록 연동
    데이터 인댁스 개수만큼 저금통 구조 생성
    저금통 컬러 %에 따라 변동하도록 연동

--- 권오성/kdt_bank/js/main.js
+++ 권오성/kdt_bank/js/main.js
@@ -10,33 +10,57 @@ dragEl.addEventListener('click', function () {
 
 
 // JSON 연동
-
-// const requestURL = '/accounts.json';
-// const request = new XMLHttpRequest();
-// request.open('GET', requestURL);
-// request.responseType = 'json';
-// request.send();
-// request.onload = function() {
-//   const accInfo = request.response;
-//   headerInfo(accInfo)
-//   accStatus(accInfo)
-// }
-
 fetch('https://05castle.github.io/team-study/accounts.json')
   .then(res => res.json())
   .then(accInfo => {
     headerInfo(accInfo),
-    accStatus(accInfo)
+    accStatus(accInfo),
+    saveList(accInfo)
   })
 
+
+  // 계좌 이름
 function headerInfo(info) {
   const h3El = document.querySelector('header h3');
   h3El.innerText = info['accounts'][0]['accountName'];
 }
 
+
+// 계좌 정보
 function accStatus(status) {
   const accNum = document.querySelector('.account_info .num strong');
   const accCash = document.querySelector('.account_info .cash');
+  const barColorEl = document.querySelector('.bar_color')
+  const barGoalEl = document.querySelector('.goal')
+  const barPer = `${status['accounts'][0]['useCash']/status['accounts'][0]['useGoal']*100}`
+  
   accNum.innerText = status['accounts'][0]['accountNum'];
-  accCash.innerText = status['accounts'][0]['accountCash'];
+  accCash.innerHTML = `${status['accounts'][0]['accountCash']} <span>원</span>`
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  barColorEl.style.width = `${barPer}%`
+  barColorEl.style.backgroundColor = `${status['accounts'][0]['useColor']}`
+  barGoalEl.style.left = `calc(${barPer}% - 4px)`
+}
+
+
+// 저금통
+function saveList(item) {
+  const saveUl = document.querySelector('.save_item')
+  for (let i = 0; i < item['accounts'][0]['saveList'].length; i++){
+    const saveLi = document.createElement('li')
+    const saveDiv = document.createElement('div')
+    const saveTitle = document.createElement('p')
+    const saveCash = document.createElement('span')
+    const savePer = `${item['accounts'][0]['saveList'][i]['saveCash']/item['accounts'][0]['saveList'][i]['saveGoal']*100}`
+  
+    saveUl.appendChild(saveLi)
+    saveLi.appendChild(saveDiv)
+    saveDiv.appendChild(saveTitle)
+    saveDiv.appendChild(saveCash)
+  
+    saveTitle.innerText = item['accounts'][0]['saveList'][i]['title']
+    saveCash.innerHTML = `${item['accounts'][0]['saveList'][i]['saveCash']}원`
+    saveDiv.style.width = `${savePer}%`
+    saveDiv.style.backgroundColor = `${item['accounts'][0]['saveList'][i]['color']}`
+  }
 }
