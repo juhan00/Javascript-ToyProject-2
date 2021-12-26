290c2370389b4e5e7df89a8a7d32530fc46eb2ac
05Castle <kos4005@gamil.com>
2021-12-27 04:29:36 +0900

    feat: 사용 내역 부분 json 연동
    사용 내역 데이터 연동
    연동된 데이터 배열에 맞춰 구조 생성
    날짜별 구조 분리
    날짜별 지출 합계 계산

--- 권오성/kdt_bank/js/main.js
+++ 권오성/kdt_bank/js/main.js
@@ -1,5 +1,4 @@
 // 드래그(영역 확장) 기능
-
 const dragEl = document.querySelector('.drag_bar')
 const hisEl = document.querySelector('.account_history')
 
@@ -10,57 +9,113 @@ dragEl.addEventListener('click', function () {
 
 
 // JSON 연동
-fetch('https://05castle.github.io/team-study/accounts.json')
+fetch('/accounts.json')
   .then(res => res.json())
-  .then(accInfo => {
-    headerInfo(accInfo),
-    accStatus(accInfo),
-    saveList(accInfo)
+  .then(accountData => {
+    startJsonInput(accountData);
   })
 
 
-  // 계좌 이름
-function headerInfo(info) {
+function startJsonInput(accData) {
+  const firstAccount = accData['accounts'][0]
+
+  // 헤더 계좌 이름 집어넣기
   const h3El = document.querySelector('header h3');
-  h3El.innerText = info['accounts'][0]['accountName'];
-}
+  h3El.innerText = firstAccount['accountName'];
 
 
-// 계좌 정보
-function accStatus(status) {
+  // 계좌 정보 반영하기
   const accNum = document.querySelector('.account_info .num strong');
   const accCash = document.querySelector('.account_info .cash');
   const barColorEl = document.querySelector('.bar_color')
-  const barGoalEl = document.querySelector('.goal')
-  const barPer = `${status['accounts'][0]['useCash']/status['accounts'][0]['useGoal']*100}`
-  
-  accNum.innerText = status['accounts'][0]['accountNum'];
-  accCash.innerHTML = `${status['accounts'][0]['accountCash']} <span>원</span>`
-  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  const barGoalEl = document.querySelector('.use_cash')
+  const barPer = `${firstAccount['useCash']/firstAccount['useGoal']*100}`
+  function addComma(won) {
+    const str = `${won}`
+    return str.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+
+  accNum.innerText = firstAccount['accountNum'];
+  accCash.innerHTML = addComma(`${firstAccount['accountCash']} <span>원</span>`)
   barColorEl.style.width = `${barPer}%`
-  barColorEl.style.backgroundColor = `${status['accounts'][0]['useColor']}`
+  barColorEl.style.backgroundColor = `${firstAccount['useColor']}`
   barGoalEl.style.left = `calc(${barPer}% - 4px)`
-}
 
 
-// 저금통
-function saveList(item) {
+  // 저금통 만들기
   const saveUl = document.querySelector('.save_item')
-  for (let i = 0; i < item['accounts'][0]['saveList'].length; i++){
+  for (let i = 0; i < firstAccount['saveList'].length; i++){
     const saveLi = document.createElement('li')
     const saveDiv = document.createElement('div')
     const saveTitle = document.createElement('p')
     const saveCash = document.createElement('span')
-    const savePer = `${item['accounts'][0]['saveList'][i]['saveCash']/item['accounts'][0]['saveList'][i]['saveGoal']*100}`
+    const savePer = `${firstAccount['saveList'][i]['saveCash']/firstAccount['saveList'][i]['saveGoal']*100}`
   
     saveUl.appendChild(saveLi)
     saveLi.appendChild(saveDiv)
     saveDiv.appendChild(saveTitle)
     saveDiv.appendChild(saveCash)
   
-    saveTitle.innerText = item['accounts'][0]['saveList'][i]['title']
-    saveCash.innerHTML = `${item['accounts'][0]['saveList'][i]['saveCash']}원`
+    saveTitle.innerText = firstAccount['saveList'][i]['title']
+    saveCash.innerHTML = `${firstAccount['saveList'][i]['saveCash']}원`
     saveDiv.style.width = `${savePer}%`
-    saveDiv.style.backgroundColor = `${item['accounts'][0]['saveList'][i]['color']}`
+    saveDiv.style.backgroundColor = `${firstAccount['saveList'][i]['color']}`
+  }
+
+
+  // 사용 내역 배열 역순
+  const historyArr = firstAccount['accountHistory']
+  const reverseArr = historyArr.reverse()
+  
+  // 사용 내역 영역 만들기
+  const recentUl = document.querySelector('.recent')
+  let day = ''
+  let listIndex = 0
+
+  for (let i = 0; i < reverseArr.length; i++) {
+    const recentHistory = reverseArr[i]['history']
+    const historyDate = reverseArr[i]['date']
+    const historyPrice = reverseArr[i]['price']
+    const incomeOrNot = reverseArr[i]['income']
+    const newRecentList = document.createElement('ul')
+    const newDayTitle = document.createElement('div')
+    const newHistory = document.createElement('li')
+    
+    // 인컴 확인하는 함수
+    function checkIncome(history) {
+      if (incomeOrNot === 'in') {
+        history.innerHTML = addComma(`${recentHistory}<span class="income">+ ${historyPrice}</span>`)
+      } else {
+        history.innerHTML = addComma(`${recentHistory}<span>${historyPrice}</span>`)
+      }
+    }
+    
+    // 해당 날짜 지출내역 추려서 합산하는 함수
+    function sumSpending (accountHistory, date) {
+      const spendHistory = accountHistory.filter(arr => arr.income === 'out' && arr.date === date)
+      const totalSpending = spendHistory.reduce((total, current) => total + current.price, 0)
+      return addComma(totalSpending)
+    }
+
+    // 사용 내역 집어넣기
+    if (day !== historyDate) { 
+      const newDayBoundary = document.createElement('li')
+      recentUl.appendChild(newDayBoundary)
+      
+      newDayBoundary.appendChild(newDayTitle)
+      newDayTitle.classList.add('recent_tit')
+      newDayTitle.innerHTML = `${historyDate}<span>${sumSpending(reverseArr, historyDate)}원 지출</span>`
+      
+      newDayBoundary.appendChild(newRecentList)
+      newRecentList.classList.add('recent_list')
+      newRecentList.appendChild(newHistory)
+      checkIncome(newHistory)
+      
+      day = historyDate
+      listIndex++
+    } else {
+      const recentList = document.querySelector(`.recent li:nth-child(${listIndex}) .recent_list`)
+      recentList.appendChild(newHistory)
+      checkIncome(newHistory)
+    }
   }
 }
